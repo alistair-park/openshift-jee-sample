2ceabb94185fddb2a892e9bcdee8f85d226d9336
Alistair Park <alistair.park@estafet.com>
2019-02-27 23:14:49 +0000

    UPdate java

---
+++ src/main/java/placement/DBSetup.java
@@ -0,0 +1,25 @@
+package placement;
+import java.sql.*;  
+
+public class DBSetup {
+
+	public static DBSetup getDBSetup() {
+		return new DBSetup();
+	}
+	public void DBSetup() {}
+	public String testConnection() {
+		StringBuffer buf = new StringBuffer();
+		try {
+			Class.forName("com.mysql.jdbc.Driver");  
+			Connection con=DriverManager.getConnection(  
+					"jdbc:mysql://localhost:3306/placement","placement","RFH2019!");  
+			//here sonoo is database name, root is username and password  
+			Statement stmt=con.createStatement();  
+			ResultSet rs=stmt.executeQuery("select * from table");  
+			while(rs.next())  
+				buf.append(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));  
+			con.close();  
+		}catch(Exception e){ System.out.println(e);}  
+		return buf.toString();
+	}  
+}
--- src/main/webapp/placement.jsp
+++ src/main/webapp/placement.jsp
@@ -4,10 +4,12 @@
 	<%@ page import="javax.servlet.http.HttpUtils,java.util.Enumeration" %>
 	<%@ page import="java.lang.management.*" %>
 	<%@ page import="java.util.*" %>
+	<%@ page import="placement.*" %>
 </HEAD>
 <BODY>
 
 <H1>WebApp JSP Test Page</H1>
+<%=DBSetup.getDBSetup().testConnection()%>
 
 </BODY>
 </HTML>
