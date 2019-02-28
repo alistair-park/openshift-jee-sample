24a5ea8087b2a89e3836628c52a88425955ddfe8
Alistair Park <alistair.park@estafet.com>
2019-02-28 12:17:17 +0000

    Update DBSetup.java

--- src/main/java/placement/DBSetup.java
+++ src/main/java/placement/DBSetup.java
@@ -3,7 +3,7 @@ import java.sql.*;
 
 
 public class DBSetup {
-	  private String server = "jdbc:mysql://" + System.getenv("OPENSHIFT_MYSQL_DB_HOST") + ":" + System.getenv("OPENSHIFT_MYSQL_DB_PORT") + "/" + "mysql";//System.getenv("OPENSHIFT_APP_NAME") + "";
+	 // private String server = "jdbc:mysql://" + System.getenv("OPENSHIFT_MYSQL_DB_HOST") + ":" + System.getenv("OPENSHIFT_MYSQL_DB_PORT") + "/" + "mysql";//System.getenv("OPENSHIFT_APP_NAME") + "";
 	    private String user = "placement";//System.getenv("OPENSHIFT_MYSQL_DB_USERNAME");
 	    private String pass = "RFH2019!";//System.getenv("OPENSHIFT_MYSQL_DB_PASSWORD");
 	    private Connection conn;
@@ -14,15 +14,16 @@ public class DBSetup {
 	public String testConnection() {
 		StringBuffer buf = new StringBuffer();
 		try {
+			String server = "jdbc:mysql://172.30.136.50:3306/mysql";
             this.conn = DriverManager.getConnection(server, user, pass);
 
 //			Connection con=DriverManager.getConnection(  
 //					"jdbc:mysql://localhost:3306/placement","placement","RFH2019!");  
 //			//here sonoo is database name, root is username and password  
 			Statement stmt=conn.createStatement();  
-			ResultSet rs=stmt.executeQuery("select * from table");  
+			ResultSet rs=stmt.executeQuery("show tables");  
 			while(rs.next())  
-				buf.append(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));  
+				buf.append(rs.getString(1));  
 			conn.close();  
 		}catch(Exception e){ buf.append(e);}//System.out.println(e);}  
 		return buf.toString();
