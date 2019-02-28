1701c2fec01d8505cc63d8ecf8673097acb05d11
Alistair Park <alistair.park@estafet.com>
2019-02-28 17:21:04 +0000

    Update DBSetup.java

--- src/main/java/placement/DBSetup.java
+++ src/main/java/placement/DBSetup.java
@@ -3,12 +3,12 @@ import java.sql.*;
 
 
 public class DBSetup {
-		private String server = "jdbc:mysql://172.30.136.50:3306/mysql";
-	    private String rootUser = "root";//System.getenv("OPENSHIFT_MYSQL_DB_USERNAME");
-	    private String rootPassword = "RFH2019!";//System.getenv("OPENSHIFT_MYSQL_DB_PASSWORD");
-	    private String appUser = "placement";//System.getenv("OPENSHIFT_MYSQL_DB_USERNAME");
-	    private String appPassword = "RFH2019!";//System.getenv("OPENSHIFT_MYSQL_DB_PASSWORD");
-	    private Connection conn;
+	private String server = "jdbc:mysql://172.30.136.50:3306/mysql";
+	private String rootUser = "root";//System.getenv("OPENSHIFT_MYSQL_DB_USERNAME");
+	private String rootPassword = "RFH2019!";//System.getenv("OPENSHIFT_MYSQL_DB_PASSWORD");
+	private String appUser = "placement";//System.getenv("OPENSHIFT_MYSQL_DB_USERNAME");
+	private String appPassword = "RFH2019!";//System.getenv("OPENSHIFT_MYSQL_DB_PASSWORD");
+	private Connection conn;
 
 	public static DBSetup getDBSetup() {
 		return new DBSetup();
@@ -27,11 +27,11 @@ public class DBSetup {
 	public String testConnection() {
 		StringBuffer buf = new StringBuffer();
 		try {
-            this.conn = DriverManager.getConnection(server, rootUser, rootPassword);
+			this.conn = DriverManager.getConnection(server, rootUser, rootPassword);
 
-//			Connection con=DriverManager.getConnection(  
-//					"jdbc:mysql://localhost:3306/placement","placement","RFH2019!");  
-//			//here sonoo is database name, root is username and password  
+			//			Connection con=DriverManager.getConnection(  
+			//					"jdbc:mysql://localhost:3306/placement","placement","RFH2019!");  
+			//			//here sonoo is database name, root is username and password  
 			Statement stmt=conn.createStatement();  
 			ResultSet rs=stmt.executeQuery("select * from REGISTRATION");  
 			while(rs.next())  
@@ -45,23 +45,41 @@ public class DBSetup {
 		// Create Student table
 		// Create Practice table
 		// Create DriveTime table
-		createStudentTable();
-	}
-	private void createStudentTable() {
 		try {
-            this.conn = DriverManager.getConnection(server, rootUser, rootPassword);
-			Statement stmt=conn.createStatement();  
-			  String sql = "CREATE TABLE REGISTRATION " +
-	                  "(id INTEGER not NULL, " +
-	                  " first VARCHAR(255), " + 
-	                  " last VARCHAR(255), " + 
-	                  " age INTEGER, " + 
-	                  " PRIMARY KEY ( id ))"; 
-
-	     stmt.executeUpdate(sql);
-	     conn.close();  
+			this.conn = DriverManager.getConnection(server, rootUser, rootPassword);
+			createStudentTable();
+			addStudentRecord(1,"Fred","Bloggs",37);
+			conn.close();  
 		}catch(Exception e){ e.printStackTrace();}  
 
+	}
 
+
+	private void addStudentRecord(int id, String firstName, String familyName, int age) throws SQLException {
+		PreparedStatement statement = conn.prepareStatement("INSERT INTO STUDENT ('id','first','last','age') VALUES (?,?,?,?)");
+
+		statement.setInt(1,id);
+		statement.setString(2, firstName);
+		statement.setString(3, familyName);
+		statement.setInt(4, age);
+
+		int affectedRows = statement.executeUpdate();
+
+		if (affectedRows == 0) {
+			throw new SQLException("Creating user failed, no rows affected.");
+		}
+	}
+
+
+	private void createStudentTable() throws SQLException {
+		Statement stmt=conn.createStatement();  
+		String sql = "CREATE TABLE REGISTRATION " +
+				"(id INTEGER not NULL, " +
+				" first VARCHAR(255), " + 
+				" last VARCHAR(255), " + 
+				" age INTEGER, " + 
+				" PRIMARY KEY ( id ))"; 
+
+		stmt.executeUpdate(sql);
 	}
 }
