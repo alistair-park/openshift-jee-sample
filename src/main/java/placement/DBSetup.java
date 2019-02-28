8d15a2c2d5d40ba21d8e231dbbd4a117f3c71810
Alistair Park <alistair.park@estafet.com>
2019-02-28 12:29:58 +0000

    Get env variables

--- src/main/java/placement/DBSetup.java
+++ src/main/java/placement/DBSetup.java
@@ -3,19 +3,28 @@ import java.sql.*;
 
 
 public class DBSetup {
-	 // private String server = "jdbc:mysql://" + System.getenv("OPENSHIFT_MYSQL_DB_HOST") + ":" + System.getenv("OPENSHIFT_MYSQL_DB_PORT") + "/" + "mysql";//System.getenv("OPENSHIFT_APP_NAME") + "";
-	    private String user = "root";//System.getenv("OPENSHIFT_MYSQL_DB_USERNAME");
-	    private String pass = "RFH2019!";//System.getenv("OPENSHIFT_MYSQL_DB_PASSWORD");
+		private String server = "jdbc:mysql://172.30.136.50:3306/mysql";
+	    private String rootUser = "root";//System.getenv("OPENSHIFT_MYSQL_DB_USERNAME");
+	    private String rootPassword = "RFH2019!";//System.getenv("OPENSHIFT_MYSQL_DB_PASSWORD");
+	    private String appUser = "placement";//System.getenv("OPENSHIFT_MYSQL_DB_USERNAME");
+	    private String appPassword = "RFH2019!";//System.getenv("OPENSHIFT_MYSQL_DB_PASSWORD");
 	    private Connection conn;
 
 	public static DBSetup getDBSetup() {
 		return new DBSetup();
 	}
+	public String getEnvVariables() {
+		StringBuffer buf = new StringBuffer();
+		buf.append("<p>" + System.getenv("MYSQL_USER") + "/p>");
+		buf.append("<p>" + System.getenv("MYSQL_PASSWORD") + "/p>");
+		buf.append("<p>" + System.getenv("MYSQL_ROOT_PASSWORD") + "/p>");
+		buf.append("<p>" + System.getenv("MYSQL_DATABASE") + "/p>");
+		return buf.toString();
+	}
 	public String testConnection() {
 		StringBuffer buf = new StringBuffer();
 		try {
-			String server = "jdbc:mysql://172.30.136.50:3306/mysql";
-            this.conn = DriverManager.getConnection(server, user, pass);
+            this.conn = DriverManager.getConnection(server, rootUser, rootPassword);
 
 //			Connection con=DriverManager.getConnection(  
 //					"jdbc:mysql://localhost:3306/placement","placement","RFH2019!");  
@@ -28,4 +37,11 @@ public class DBSetup {
 		}catch(Exception e){ buf.append(e);}//System.out.println(e);}  
 		return buf.toString();
 	}  
+	public void initialiseDatabase() {
+		// Create user
+		// Grant permissions
+		// Create Student table
+		// Create Practice table
+		// Create DriveTime table
+	}
 }
