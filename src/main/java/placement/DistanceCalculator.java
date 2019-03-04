c413272f286ac9e38ba898a2f40cb818495b9d6b
Alistair Park <alistair.park@estafet.com>
2019-03-04 17:11:33 +0000

    Update DistanceCalculator.java

--- src/main/java/placement/DistanceCalculator.java
+++ src/main/java/placement/DistanceCalculator.java
@@ -11,15 +11,44 @@ class DistanceCalculator
 {
 	String key;
 	String mode;
+	
+	public static void main(String[] args) {
+		DistanceCalculator dc = new DistanceCalculator("AIzaSyAt08icyRLfS-8bGNNaqqJmg1r4UPuPkjs","walking");
+		dc.getDistance("HA53YJ", "NW32UB");
+		dc.getDistance("W1D4LR", "NW32UB");
+		dc.getDistance("NW32UB", "W1D4LR");
+		dc.getDistance("HA53YJ", "SE91JE");
+		dc.getDistance("W1D 4LR", "SE9 1JE");
+		dc.getDistance("SE91JE", "W1D4LR");
+	}
 	public DistanceCalculator(String key, String mode) {
 		this.key = key;
 		this.mode = mode;
 	}
-	public long getDistance(String fromPostcode, String toPostcode)
+		
+	public long getDistance(String postcode1, String postcode2)
 	{
 		//inline will store the JSON data streamed in string format
 		String inline = "";
 		long distance = 0;
+		postcode1 = postcode1.toUpperCase().replaceAll("\\s+","");
+		postcode2 = postcode2.toUpperCase().replaceAll("\\s+","");
+		int comparison = postcode1.compareTo(postcode2);
+		String fromPostcode;
+		String toPostcode;
+		System.out.println("Comparison =" + comparison);
+		if (comparison == 0) {
+			return 1; // don't return 0 as that is "not found"
+		}
+		else if (comparison > 0) {
+			fromPostcode = postcode2;
+			toPostcode = postcode1;
+		}
+		else {
+			fromPostcode = postcode1;
+			toPostcode = postcode2;
+		}
+		System.out.println(fromPostcode + "->" + toPostcode);
 	
 		try
 		{
@@ -33,11 +62,13 @@ class DistanceCalculator
 			conn.connect();
 			//Get the response status of the Rest API
 			int responsecode = conn.getResponseCode();
-			System.out.println("Response code is: " +responsecode);
+//			System.out.println("Response code is: " +responsecode);
 			
 			//Iterating condition to if response code is not 200 then throw a runtime exception
 			//else continue the actual process of getting the JSON data
 			if(responsecode != 200) {
+				System.out.println("distance [NOT FOUND]");
+
 				return 0;
 			}
 			else
@@ -48,8 +79,8 @@ class DistanceCalculator
 				{
 					inline+=sc.nextLine();
 				}
-				System.out.println("\nJSON Response in String format"); 
-				System.out.println(inline);
+//				System.out.println("\nJSON Response in String format"); 
+//				System.out.println(inline);
 				//Close the stream when reading the data has been finished
 				sc.close();
 			}
@@ -70,7 +101,9 @@ class DistanceCalculator
 		}
 		catch(Exception e)
 		{
-			e.printStackTrace();
+//			e.printStackTrace();
+			System.out.println("distance [ERROR]");
+
 		}
 		return distance;
 	}
